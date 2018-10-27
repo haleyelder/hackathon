ed986182554e4bd01f98d72f2fb4c5680112f29f
Nikki Wong <nikkiwong@miami.edu>
2018-10-27 16:22:32 -0700

    update main.js

--- src/main.js
+++ src/main.js
@@ -4,6 +4,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(function() {
+    $("#nameForm").submit(function() {
+    event.preventDefault();
+    const name = $('input[type="text"]').val();
+    $(".envelope").show();
+    $("#nameForm").hide();
+    $(".envelope").click(function() {
+      $('#student-name').text(name);
+      $('.invitation').show();
+      $('.envelope').hide();
+    })
+  })
 });
 
 
